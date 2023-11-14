de0ca23a7a2545bd9d41b1cc1a623b9cf994dadc
Cortecci00 <leonel.cortecci@davinci.edu.ar>
2023-11-13 21:58:33 -0300

    Ahora si
    ahora si

---
+++ src/test/java/ar/edu/davinci/dvds20232cg7/DvDs20232cG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20232cg7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20232cG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
