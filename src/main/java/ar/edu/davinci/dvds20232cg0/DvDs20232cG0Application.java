efa6657193ba904259f9cb35787dbaced58c6199
Cortecci00 <leonel.cortecci@davinci.edu.ar>
2023-11-13 21:35:28 -0300

    Probando
    Esperemos que funciones

---
+++ src/main/java/ar/edu/davinci/dvds20232cg0/DvDs20232cG0Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20232cg0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20232cG0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20232cG0Application.class, args);
+	}
+
+}
